68b2617af5a743ddbccdbb75fca2cb233e1a164e
Burak Örkmez <burakorkmezz@gmail.com>
2021-11-19 22:41:53 +0100

    added useSignup hook

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import { projectAuth, projectStorage } from '../firebase/config';
+import { useAuthContext } from './useAuthContext';
+
+export const useSignup = () => {
+	const [isCancelled, setIsCancelled] = useState(false);
+	const [isPending, setIsPending] = useState(false);
+	const [error, setError] = useState(null);
+
+	const { dispatch } = useAuthContext();
+
+	const signup = async (email, password, displayName, thumbnail) => {
+		setError(null);
+		setIsPending(true);
+
+		try {
+			// signup
+			const res = await projectAuth.createUserWithEmailAndPassword(
+				email,
+				password
+			);
+
+			if (!res) {
+				throw new Error('Could not complete signup');
+			}
+
+			const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`;
+			const img = await projectStorage.ref(uploadPath).put(thumbnail);
+			const imgUrl = await img.ref.getDownloadURL();
+
+			// add display name to user
+			await res.user.updateProfile({ displayName, photoURL: imgUrl });
+
+			// login action
+			dispatch({ type: 'LOGIN', payload: res.user });
+
+			if (!isCancelled) {
+				setIsPending(false);
+				setError(null);
+			}
+		} catch (err) {
+			setIsPending(false);
+			setError(err.message);
+		}
+	};
+
+	useEffect(() => {
+		return () => setIsCancelled(true);
+	}, []);
+
+	return { signup, isPending, error };
+};
