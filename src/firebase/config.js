4e233db3c1b83ca0fa46a86a899ad57157c3a2d4
Burak Örkmez <burakorkmezz@gmail.com>
2021-11-19 18:48:15 +0100

    added firebase storage and signup page

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyDCtswQUDFqXKf5ikYdtvRALz3frzN36qA',
@@ -16,7 +17,9 @@ firebase.initializeApp(firebaseConfig);
 
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
+// timestamp
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, projectStorage, timestamp };
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,79 @@
+import { useState } from 'react';
+
+// styles
 import './Signup.css';
 
 export default function Signup() {
-	return <div>Signup </div>;
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [displayName, setDisplayName] = useState('');
+	const [thumbnail, setThumbnail] = useState(null);
+	const [thumbnailError, setThumbnailError] = useState(null);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(email, password, displayName);
+	};
+
+	const handleFileChange = (e) => {
+		setThumbnail(null);
+		let selectedFile = e.target.files[0];
+		console.log(selectedFile);
+
+		if (!selectedFile) {
+			setThumbnailError('Please select a file');
+			return;
+		}
+		if (!selectedFile.type.includes('image')) {
+			setThumbnailError('Selected file must be an image');
+			return;
+		}
+		if (selectedFile.size > 100000) {
+			setThumbnailError('Image file size must be less than 100kb');
+			return;
+		}
+
+		setThumbnailError(null);
+		setThumbnail(selectedFile);
+		console.log('thumbnail updated');
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className="auth-form">
+			<h2>sign up</h2>
+			<label>
+				<span>email:</span>
+				<input
+					required
+					type="email"
+					onChange={(e) => setEmail(e.target.value)}
+					value={email}
+				/>
+			</label>
+			<label>
+				<span>password:</span>
+				<input
+					required
+					type="password"
+					onChange={(e) => setPassword(e.target.value)}
+					value={password}
+				/>
+			</label>
+			<label>
+				<span>display name:</span>
+				<input
+					required
+					type="text"
+					onChange={(e) => setDisplayName(e.target.value)}
+					value={displayName}
+				/>
+			</label>
+			<label>
+				<span>Profile thumbnail:</span>
+				<input required type="file" onChange={handleFileChange} />
+				{thumbnailError && <div className="error">{thumbnailError}</div>}
+			</label>
+			<button className="btn">Sign up</button>
+		</form>
+	);
 }
