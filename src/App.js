c513ac96b1811e83a72f88b99d4f70cc510a2677
Burak Örkmez <burakorkmezz@gmail.com>
2021-11-20 02:09:44 +0100

    added route guards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 // styles
 import './App.css';
@@ -9,38 +9,44 @@ import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/navbar/Navbar';
 import Sidebar from './components/sidebar/Sidebar';
+import { useAuthContext } from './hooks/useAuthContext';
 
 // pages and components
 
 function App() {
+	const { user, authIsReady } = useAuthContext();
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<Sidebar />
-				<div className="container">
-					<Navbar />
-					<Switch>
-						<Route exact path="/">
-							<Dashboard />
-						</Route>
-						<Route path="/create">
-							<Create />
-						</Route>
-						<Route path="/projects/:id">
-							<Project />
-						</Route>
-						<Route exact path="/">
-							<Dashboard />
-						</Route>
-						<Route path="/login">
-							<Login />
-						</Route>
-						<Route path="/signup">
-							<Signup />
-						</Route>
-					</Switch>
-				</div>
-			</BrowserRouter>
+			{authIsReady && (
+				<BrowserRouter>
+					<Sidebar />
+					<div className="container">
+						<Navbar />
+						<Switch>
+							<Route exact path="/">
+								{user && <Dashboard />}
+								{!user && <Redirect to="/login" />}
+							</Route>
+							<Route path="/create">
+								{user && <Create />}
+								{!user && <Redirect to="/login" />}
+							</Route>
+							<Route path="/projects/:id">
+								{user && <Project />}
+								{!user && <Redirect to="/login" />}
+							</Route>
+							<Route path="/login">
+								{user && <Redirect to="/" />}
+								{!user && <Login />}
+							</Route>
+							<Route path="/signup">
+								{user && <Redirect to="/" />}
+								{!user && <Signup />}
+							</Route>
+						</Switch>
+					</div>
+				</BrowserRouter>
+			)}
 		</div>
 	);
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,9 +4,11 @@ import { useLogout } from '../../hooks/useLogout';
 // styles & images
 import './Navbar.css';
 import Temple from '../../assets/temple.svg';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 export default function Navbar() {
 	const { logout, isPending, error } = useLogout();
+	const { user } = useAuthContext();
 
 	return (
 		<div className="navbar">
@@ -15,25 +17,30 @@ export default function Navbar() {
 					<img src={Temple} alt="logo" />
 					<span>Sunday</span>
 				</li>
-
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-				<li>
-					<Link to="/signup">Signup</Link>
-				</li>
-				<li>
-					{!isPending && (
-						<button className="btn" onClick={logout}>
-							Logout
-						</button>
-					)}
-					{isPending && (
-						<button className="btn" onClick={logout}>
-							Logging out...
-						</button>
-					)}
-				</li>
+				{!user && (
+					<>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+						<li>
+							<Link to="/signup">Signup</Link>
+						</li>
+					</>
+				)}
+				{user && (
+					<li>
+						{!isPending && (
+							<button className="btn" onClick={logout}>
+								Logout
+							</button>
+						)}
+						{isPending && (
+							<button className="btn" onClick={logout}>
+								Logging out...
+							</button>
+						)}
+					</li>
+				)}
 				{error && <li>{error}</li>}
 			</ul>
 		</div>
